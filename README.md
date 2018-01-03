d69cceb9c97ad75095853454d5eaca198f5b230a
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2018-01-03 15:46:13 +0100

    adding dev files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# dataglide
-Analyse data from gliders
+# FFVL flights records
+https://parapente.ffvl.fr/cfd/selectionner-les-vols
+
+https://parapente.ffvl.fr/sites/parapente.ffvl.fr/files/igcfiles/2017-12-20-igcfile-95927-139418.igc   
+
+crawl bitch!
+
+
+# IGC file format
+http://www.ukiws.demon.co.uk/GFAC/documents/tech_spec_gnss.pdf
---
+++ igc.py
@@ -0,0 +1,45 @@
+import time
+
+
+
+
+class igc_trace():
+    def __init__(self):
+        self.file = ''
+        self.pilot = ''
+        self.trace={
+            'times' : [],
+            'latitudes' : [],
+            'longitudes': [],
+            'altitudes_p' : [],
+            'altitudes_gps' : [],
+        }
+    def read_igc_file(self, filename):
+        self.file = filename
+        f = open(filename, "r")
+        data = f.read().splitlines()
+        f.close()
+        if len(data[3].split(':')) > 1:
+            self.pilot = data[3].split(':')[1]
+        for d in data:
+            if d[0] == 'B':
+                igc_B = read_igc_B(d)
+                self.trace['times'].append(igc_B['time'])
+                self.trace['latitudes'].append(igc_B['lat'])
+                self.trace['longitudes'].append(igc_B['long'])
+                self.trace['altitudes_p'].append(igc_B['alt_p'])
+                self.trace['altitudes_gps'].append(igc_B['alt_gps'])
+                
+                
+                
+def read_igc_B(line):
+    lat = line[7:15]
+    long = line[15:24]
+    dic = {'time': time.strptime(line[1:7], '%H%M%S'), 
+           'lat': (-1*(lat[-1]=='S') + 1*(lat[-1]=='N')) * (int(lat[:2]) + (float(lat[2:4])+float(lat[4:-1])/1000.)/60), 
+           'long': (-1*(long[-1]=='W') + 1*(long[-1]=='E')) * (int(long[:3]) + (float(long[3:5])+float(long[5:-1])/1000.)/60)
+          }
+    if 'A' in line:
+        dic['alt_p'] = float(line[25:30])
+        dic['alt_gps'] = float(line[30:35])
+    return dic
